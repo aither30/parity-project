aaee0ed9e494fb6bf23905d48beefa21bafbae5f
aither <alpidarulhakim05@gmail.com>
2025-02-07 03:27:47 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.cache = false; // Menonaktifkan caching Webpack untuk menghindari masalah memori
-    return config;
-  },
-  images: {
-    domains: ["cdn.jsdelivr.net"],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
